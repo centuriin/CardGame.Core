1aed4c214b6986de67bc04be3449becc72547885
centuriin <centuriin@yandex.ru>
2024-03-23 15:33:07 +0300

    add default card description

---
+++ CardGame.Core.Tests/DefaultCard/DefaultCardDescription.Tests.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+
+using CardGame.Core.DefaultCard;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace CardGame.Core.Tests.DefaultCard;
+
+public class DefaultCardDescriptionTests
+{
+    [Fact(DisplayName = "Can create.")]
+    [Trait("Category", "Unit")]
+    public void CanCreate()
+    {
+        // Arrange
+        var suit = CardSuit.Diamonds;
+        var type = CardType.Queen;
+
+        // Act
+        var descritpion = new DefaultCardDescription(suit, type);
+
+        // Assert
+        descritpion.Suit.Should().Be(suit);
+        descritpion.Type.Should().Be(type);
+    }
+
+    [Fact(DisplayName = "Cannot create with invalid suit.")]
+    [Trait("Category", "Unit")]
+    public void CanNotCreateWithInvalidSuit()
+    {
+        // Arrange
+        var suit = (CardSuit)byte.MaxValue;
+        var type = CardType.Queen;
+
+        // Act
+        var exception = Record.Exception(() =>
+            new DefaultCardDescription(suit, type));
+
+        // Assert
+        exception.Should().BeOfType<InvalidEnumArgumentException>();
+    }
+
+    [Fact(DisplayName = "Cannot create with invalid card type.")]
+    [Trait("Category", "Unit")]
+    public void CanNotCreateWithInvalidCardType()
+    {
+        // Arrange
+        var suit = CardSuit.Diamonds;
+        var type = (CardType)byte.MaxValue;
+
+        // Act
+        var exception = Record.Exception(() =>
+            new DefaultCardDescription(suit, type));
+
+        // Assert
+        exception.Should().BeOfType<InvalidEnumArgumentException>();
+    }
+
+    [Theory(DisplayName = "Can equal description.")]
+    [MemberData(nameof(EqualsData))]
+    [Trait("Category", "Unit")]
+    public void CanEqualDescription(
+        DefaultCardDescription first,
+        DefaultCardDescription second,
+        bool expectedResult)
+    {
+        // Act
+        var actual = first.Equals(second);
+
+        // Assert
+        actual.Should().Be(expectedResult);
+    }
+
+    public static TheoryData<DefaultCardDescription, DefaultCardDescription, bool> EqualsData = new()
+    {
+        {
+            new DefaultCardDescription(CardSuit.Diamonds, CardType.Two),
+            new DefaultCardDescription(CardSuit.Diamonds, CardType.Two),
+            true
+        },
+        {
+            new DefaultCardDescription(CardSuit.Diamonds, CardType.Jack),
+            new DefaultCardDescription(CardSuit.Diamonds, CardType.Two),
+            false
+        },
+        {
+            new DefaultCardDescription(CardSuit.Spades, CardType.Two),
+            new DefaultCardDescription(CardSuit.Diamonds, CardType.Two),
+            false
+        },
+    };
+}
--- CardGame.Core/DefaultCard/DefaultCardDescription.cs
+++ CardGame.Core/DefaultCard/DefaultCardDescription.cs
@@ -2,12 +2,34 @@
 
 namespace CardGame.Core.DefaultCard;
 
+/// <summary>
+/// Представляет описание обычных игральных карт от 
+/// <see cref="CardType.Two"> до <see cref="CardType.Ace"> четырех мастей <see cref="CardSuit">.
+/// </summary>
 public class DefaultCardDescription : ICardDescription
 {
+    /// <summary>
+    /// Масть карты.
+    /// </summary>
     public CardSuit Suit { get; }
 
+    /// <summary>
+    /// Тип карты.
+    /// </summary>
     public CardType Type { get; }
 
+    /// <summary>
+    /// Создает новый объект типа <see cref="DefaultCardDescription"/>.
+    /// </summary>
+    /// <param name="suit">
+    /// Масть карты.
+    /// </param>
+    /// <param name="type">
+    /// Тип карты.
+    /// </param>
+    /// <exception cref="InvalidEnumArgumentException">
+    /// Если перечисления имели неверные значения.
+    /// </exception>
     public DefaultCardDescription(CardSuit suit, CardType type)
     {
         if (!Enum.IsDefined(suit))
@@ -23,6 +45,7 @@ public class DefaultCardDescription : ICardDescription
         Type = type;
     }
 
+    /// <inheritdoc/>
     public bool Equals(ICardDescription? other) => 
         other is DefaultCardDescription description &&
         description.Suit == Suit &&
