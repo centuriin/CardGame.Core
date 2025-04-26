ef9c8d8f791be5152f267be5391256b6005edc43
centuriin <centuriin@yandex.ru>
2025-04-26 13:03:39 +0300

    added dispatcher

---
+++ CardGame.Core.Tests/Engine/EventDispatcher{TEvent}.Tests.cs
@@ -0,0 +1,44 @@
+﻿using Centuriin.CardGame.Core.Engine;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Centuriin.CardGame.Core.Tests.Engine;
+
+public sealed class EventDispatherTests
+{
+    [Fact]
+    public async Task CanRegisterAndPublishAsync()
+    {
+        // Arrange
+        var action1Calls = 0;
+        var action1 = (FakeEvent _, CancellationToken _) =>
+        {
+            action1Calls++;
+            return Task.CompletedTask;
+        };
+
+        var action2Calls = 0;
+        var action2 = (FakeDerivedEvent _, CancellationToken _) =>
+        {
+            action2Calls++;
+            return Task.CompletedTask;
+        };
+
+        var dispatcher = new EventDispatcher<FakeEvent>();
+
+        dispatcher.Register(action1);
+        dispatcher.Register(action2);
+
+        // Act
+        await dispatcher.PublishAsync(new FakeDerivedEvent(), CancellationToken.None);
+
+        // Assert
+        action1Calls.Should().Be(1);
+        action2Calls.Should().Be(1);
+    }
+
+    public class FakeEvent;
+    public sealed class FakeDerivedEvent : FakeEvent;
+}
---
+++ CardGame.Core/Engine/EventDispatcher{TEvent}.cs
@@ -0,0 +1,47 @@
+﻿namespace Centuriin.CardGame.Core.Engine;
+
+public sealed class EventDispatcher<TEvent> : IEventDispatcher<TEvent>
+{
+    private readonly Dictionary<Type, List<Delegate>> _actions = [];
+
+    public void Register<TEvent1>(Func<TEvent1, CancellationToken, Task> action)
+        where TEvent1 : TEvent
+    {
+        ArgumentNullException.ThrowIfNull(action);
+
+        if (_actions.ContainsKey(typeof(TEvent1)))
+        {
+            _actions[typeof(TEvent1)].Add(action);
+        }
+        else
+        {
+            _actions.Add(typeof(TEvent1), [action]);
+        }
+    }
+
+    public void Unregister<TEvent1>(Func<TEvent1, CancellationToken, Task> action)
+        where TEvent1 : TEvent
+    {
+        ArgumentNullException.ThrowIfNull(action);
+
+        if (_actions.ContainsKey(typeof(TEvent1)))
+        {
+            _actions[typeof(TEvent1)].Remove(action);
+        }
+    }
+
+    public async Task PublishAsync<TEvent1>(TEvent1 published, CancellationToken token)
+        where TEvent1 : TEvent
+    {
+        ArgumentNullException.ThrowIfNull(published);
+
+        token.ThrowIfCancellationRequested();
+
+        await Task.WhenAll(_actions
+            .Where(x => x.Key.IsAssignableFrom(typeof(TEvent1)))
+            .SelectMany(x => x.Value)
+            .Cast<Func<TEvent1, CancellationToken, Task>>()
+            .Select(x => x.Invoke(published, token)));
+    }
+
+}
